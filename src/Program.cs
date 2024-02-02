6f81aaeafa560968a5b2d69f70ca603087e520f5
polatengin <enpolat@microsoft.com>
2024-02-02 22:28:42 +0000

    creating azure search client with search service endpoint and demo index name

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,7 @@
-using Azure;
-using Azure.Search.Documents;
-
 var endpointEnvironmentVariable = Environment.GetEnvironmentVariable("SEARCH_ENDPOINT") ?? "";
 var apiKeyEnvironmentVariable = Environment.GetEnvironmentVariable("SEARCH_API_KEY") ?? "";
 
+var indexName = "demo_index";
+
 var credential = new AzureKeyCredential(apiKeyEnvironmentVariable);
+var client = new SearchClient(new Uri(endpointEnvironmentVariable), indexName, credential);
