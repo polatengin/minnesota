39b716eaba9d9fa6d2305a6de9547e9bf876229d
polatengin <enpolat@microsoft.com>
2024-02-02 02:59:29 +0000

    creating resource group

--- deploy_infra.sh
+++ deploy_infra.sh
@@ -1,5 +1,6 @@
 PROJECT_PREFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 6 | head -n 1)
 LOCATION="westus"
+
 if [ -f .minnesota.json ]; then
   PROJECT_PREFIX=$(jq -r '.project_prefix' .minnesota.json)
   LOCATION=$(jq -r '.location' .minnesota.json)
@@ -7,3 +8,4 @@ else
   echo "{\"project_prefix\": \"$PROJECT_PREFIX\", \"location\": \"$LOCATION\"}" > .minnesota.json
 fi
 
+az group create --name "${PROJECT_PREFIX}-rg" --location "${LOCATION}"
