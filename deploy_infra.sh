37afa7d10b51677098b7f8e5a6f849b4df4f419c
polatengin <enpolat@microsoft.com>
2024-02-02 02:58:55 +0000

    if .minnesota.json exists, use it to fill variables

--- deploy_infra.sh
+++ deploy_infra.sh
@@ -1,2 +1,9 @@
 PROJECT_PREFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 6 | head -n 1)
 LOCATION="westus"
+if [ -f .minnesota.json ]; then
+  PROJECT_PREFIX=$(jq -r '.project_prefix' .minnesota.json)
+  LOCATION=$(jq -r '.location' .minnesota.json)
+else
+  echo "{\"project_prefix\": \"$PROJECT_PREFIX\", \"location\": \"$LOCATION\"}" > .minnesota.json
+fi
+
