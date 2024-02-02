d0d1cd90f993adde0a84615f710ca6b9bb5cd5d8
polatengin <enpolat@microsoft.com>
2024-02-02 03:00:06 +0000

    adding search endpoint and admin key retrieval

--- deploy_infra.sh
+++ deploy_infra.sh
@@ -10,3 +10,6 @@ fi
 
 az group create --name "${PROJECT_PREFIX}-rg" --location "${LOCATION}"
 az search service create --name "${PROJECT_PREFIX}-search" --resource-group "${PROJECT_PREFIX}-rg" --sku "free" --location "${LOCATION}"
+
+export SEARCH_ENDPOINT=$(az search service show --name "${PROJECT_PREFIX}-search" --resource-group "${PROJECT_PREFIX}-rg" --query "endpoint" -o "tsv")
+export SEARCH_ADMIN_KEY=$(az search admin-key show --resource-group "${PROJECT_PREFIX}-rg" --service-name "${PROJECT_PREFIX}-search" --query "primaryKey" -o "tsv")
