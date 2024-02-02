619c9834e6baced30e0182830f92c68f575910b1
polatengin <enpolat@microsoft.com>
2024-02-02 23:23:35 +0000

    checking if the demo index is exists

--- src/Program.cs
+++ src/Program.cs
@@ -6,4 +6,10 @@ var indexName = "demo_index";
 var credential = new AzureKeyCredential(adminKeyEnvironmentVariable);
 var indexClient = new SearchIndexClient(new Uri(endpointEnvironmentVariable), credential);
 
+var index = (await indexClient.GetIndexAsync(indexName)).Value;
+
+if (index == null)
+{
+}
+
 var client = new SearchClient(new Uri(endpointEnvironmentVariable), indexName, credential);
