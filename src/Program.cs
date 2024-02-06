900a8ed1ee0bf837da97df8df5af2f59c4e788d9
polatengin <enpolat@microsoft.com>
2024-02-06 20:16:22 +0000

    implementing elasticsearch

--- src/Program.cs
+++ src/Program.cs
@@ -1,63 +1,24 @@
-﻿var endpointEnvironmentVariable = Environment.GetEnvironmentVariable("SEARCH_ENDPOINT") ?? "";
-var adminKeyEnvironmentVariable = Environment.GetEnvironmentVariable("SEARCH_ADMIN_KEY") ?? "";
+﻿var settings = new ElasticsearchClientSettings(new Uri("https://localhost:9200"))
+  .CertificateFingerprint(Environment.GetEnvironmentVariable("CERTIFICATE_FINGERPRINT") ?? string.Empty)
+  .Authentication(new BasicAuthentication("elastic", Environment.GetEnvironmentVariable("ELASTIC_PASSWORD") ?? string.Empty))
+  .DisablePing()
+  .DisableDirectStreaming(true)
+  .SniffOnStartup(false)
+  .SniffOnConnectionFault(false);;
+
+var client = new ElasticsearchClient(settings);
 
 var indexName = $"demo-index-{Random.Shared.Next(0, 1000)}";
 
-var credential = new AzureKeyCredential(adminKeyEnvironmentVariable);
+var jsonContent = File.ReadAllText("data.json");
 
-var client = new SearchClient(new Uri(endpointEnvironmentVariable), indexName, credential);
+var contentList = JsonSerializer.Deserialize<List<ContentItem>>(jsonContent) ?? new List<ContentItem>();
 
-var indexClient = new SearchIndexClient(new Uri(endpointEnvironmentVariable), credential);
+Console.WriteLine($"Uploading {contentList.Count} documents to the {indexName}...");
 
-var indexes = indexClient.GetIndexNames().Where(e => e == indexName).ToList();
-
-if (indexes.Count == 0 || indexes.Exists(e => e != indexName))
+foreach (var item in contentList)
 {
-  var index = new SearchIndex(indexName)
-  {
-    Fields =
-    {
-      new SimpleField("id", SearchFieldDataType.String) { IsKey = true, IsFilterable = true, IsSortable = true },
-        new SimpleField("idForSort", SearchFieldDataType.Int64) { IsSortable = true },
-        new SearchableField("alias") { IsFilterable = true, AnalyzerName = LexicalAnalyzerName.EnLucene },
-        new SimpleField("contentCategoryType", SearchFieldDataType.Int32) { IsFilterable = true },
-        new SimpleField("contentCategoryTypeName", SearchFieldDataType.String),
-        new SimpleField("countryId", SearchFieldDataType.Int32) { IsFilterable = true },
-        new SimpleField("countryName", SearchFieldDataType.String),
-        new SimpleField("cityName", SearchFieldDataType.String) { IsFilterable = true },
-        new SimpleField("coverFileName", SearchFieldDataType.String),
-        new SimpleField("coverFilePath", SearchFieldDataType.String),
-        new SimpleField("displayOrder", SearchFieldDataType.Int32) { IsSortable = true },
-        new SimpleField("seoUrl", SearchFieldDataType.String),
-        new SimpleField("shortDescription", SearchFieldDataType.String),
-        new SimpleField("status", SearchFieldDataType.Boolean) { IsFilterable = true },
-        new SearchableField("title") { IsFilterable = true, IsSortable = true, AnalyzerName = LexicalAnalyzerName.EnLucene },
-        new SimpleField("treatmentId", SearchFieldDataType.Int64) { IsFilterable = true },
-        new SimpleField("treatmentName", SearchFieldDataType.String),
-        new SimpleField("isHighlightContent", SearchFieldDataType.Boolean) { IsFilterable = true },
-        new SimpleField("isShowInPage", SearchFieldDataType.Boolean) { IsFilterable = true, IsFacetable = true },
-        new SimpleField("language", SearchFieldDataType.String),
-        new SimpleField("categoryId", SearchFieldDataType.Int64) { IsFilterable = true },
-        new SimpleField("categoryName", SearchFieldDataType.String),
-        new SimpleField("countrySeoName", SearchFieldDataType.String) { IsFilterable = true },
-        new SimpleField("citySeoName", SearchFieldDataType.String) { IsFilterable = true }
-    }
-  };
-
-  index.Suggesters.Add(new SearchSuggester("sg", new List<string> { "title" }));
-
-  await indexClient.CreateIndexAsync(index);
-
-  var jsonContent = File.ReadAllText("data.json");
-
-  var contentList = JsonSerializer.Deserialize<List<ContentItem>>(jsonContent) ?? new List<ContentItem>();
-
-  Console.WriteLine($"Uploading {contentList.Count} documents to the {indexName}...");
-
-  foreach (var item in contentList)
-  {
-    await client.IndexDocumentsAsync(IndexDocumentsBatch.Upload(new[] { item }));
-  }
+  await client.IndexAsync(item, indexName);
 }
 
 do {
@@ -71,17 +32,29 @@ do {
     break;
   }
 
-  SearchResults<SearchDocument> response = client.Search<SearchDocument>(search + "*", new SearchOptions { IncludeTotalCount = true, SearchMode = SearchMode.All, QueryType = SearchQueryType.Full });
+var response = await client.SearchAsync<ContentItem>(s => s
+  .Index(indexName)
+  .From(0)
+  .Size(1000)
+  .Query(q => q
+    .Bool(b => b
+      .Should(
+        bs => bs.Match(m => m.Field(f => f.Title).Query(search.ToLowerInvariant().Trim())),
+        bs => bs.Match(m => m.Field(f => f.CoverFilePath).Query(search.ToLowerInvariant().Trim()))
+      )
+    )
+  )
+);
 
-  var totalCount = response.TotalCount;
+  var totalCount = response.Documents.Count;
 
   Console.SetCursorPosition(search.Length + 40, Console.CursorTop - 1);
   Console.WriteLine($" (Total count: {totalCount})");
 
-  foreach (var result in response.GetResults().Select(e => e.Document))
+  foreach (var result in response.Documents)
   {
     Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine($"* {result.GetString("title")}");
+    Console.WriteLine($"* {result.Title}");
   }
 } while (true);
 
